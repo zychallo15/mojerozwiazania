b17a8d23320a0e11af0291830c1f710a28c25449
zychallo15 <zychallo15@gmail.com>
2018-03-04 21:57:22 +0100

    test

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,13 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if(value==0){
+      return 1;
+  }else if(value>0 && value<13){
+      return value*factorial(value-1);
+  }else if(value<0 && value>-13){
+      return value*factorial(value+1);
+  }else{
+      return 0;
+  }
 }
